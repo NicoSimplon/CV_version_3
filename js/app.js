06ca3e142e1d968081df3e49a8cf37ab68c287c5
NicoSimplon <marty.nicolas11@laposte.net>
2018-02-19 23:48:52 +0100

    cv responsive mais overlay non-fonctionnel

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//Fonction permettant la transformation du menu sur support mobile
+
+function menuNavMobile() {
+    var x = document.getElementById("menuNavMob");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+//Fonctions gérant l'affichage des compétences dans l'overlay
+
+$("#C1").click(function(){
+	$("#titre_overlay").text('Compétences (en cours d\'acquisition)');
+	$("#liste_overlay").html('<li>Capacité à maquetter une application ;</li><br/><li>Capacité à concevoir une base de données ;</li><br/><li>Capacité à mettre en place une base de données ;</li><br/><li>Capacité à développer une interface utilisateur ;</li><br/><li>Capacité à développer des composants d\'accès aux données ;</li><br/></ul>');
+});
+
+$("#C2").click(function(){
+	$("#titre_overlay").text('Compétences (en cours d\'acquisition)');
+	$("#liste_overlay").html('<li>Capacité à développer des pages web en lien avec une base de données ;</li><br/><li>Capacité à mettre en œuvre une solution de gestion de contenu ou e-commerce ;</li><br/><li>Capacité à développer une application simple de mobilité numérique ;</li><br/><li>Capacité à utiliser l\’anglais dans son activité professionnelle en informatique ;</li><br/>);
+});
